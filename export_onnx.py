b93c79a986289d6a584b4cd3722d0bdaffb151e0
yuxin28 <93987498+yuxin28@users.noreply.github.com>
2023-07-10 14:19:32 +0200

    Add files via upload

---
+++ export_onnx.py
@@ -0,0 +1,13 @@
+import torch as t
+from trainer import Trainer
+import sys
+import torchvision as tv
+import model
+
+epoch = int(sys.argv[-1])
+#TODO: Enter your model here
+model = model.ResNet()
+crit = t.nn.BCELoss()
+trainer = Trainer(model, crit)
+trainer.restore_checkpoint(epoch)
+trainer.save_onnx('checkpoint_{:03d}.onnx'.format(epoch))
